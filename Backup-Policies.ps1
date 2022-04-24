9b7bfb04e2b5f6c588a176ce1e7263274fc87681
sandeshyapuram <sandeshyapuram@gmail.com>
2022-04-24 18:17:10 +0530

    sss

---
+++ Backup-Policies.ps1
@@ -0,0 +1,15 @@
+# This is to retrieve/backup all the Conditional Access Policies
+Install-Module Microsoft.Graph.Identity.SignIns -Scope CurrentUser -Force
+Import-Module Microsoft.Graph.Identity.SignIns -Force
+Connect-MgGraph
+
+$AllPolicies = Get-MgIdentityConditionalAccessPolicy
+
+foreach ($Policy in $AllPolicies) {
+
+    Write-Output "Backing up $($Policy.DisplayName)"
+
+    $PolicyJSON = $Policy | ConvertTo-Json -Depth 6
+
+    $PolicyJSON | Out-File ".\$($Policy.DisplayName).json"
+}
