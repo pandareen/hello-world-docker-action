b8106a12023210e00b0bdd4c3251f5e1e4dd4156
sandeshyapuram <sandeshyapuram@gmail.com>
2022-05-13 16:55:41 +0530

    Update delete_contacts.ps1

--- delete_contacts.ps1
+++ delete_contacts.ps1
@@ -18,7 +18,7 @@ $ConnectGraph = Invoke-RestMethod -Uri https://login.microsoftonline.com/$Tenant
 $token = $ConnectGraph.access_token
 Write-Output "The token $token has been processed."
 
-$BackupJsons = Get-ChildItem $BackupPath -Recurse -Include *.json
+$BackupJsons = Get-Item -Recurse -Include *.json
 foreach ($Json in $BackupJsons) {
     
     $json = Get-Content $Json.FullName | Out-String | ConvertFrom-Json
@@ -26,16 +26,5 @@ foreach ($Json in $BackupJsons) {
     $json.PSObject.Properties.Remove('modifiedDateTime')
     Write-Output "The json is $($json|ConvertTo-Json) ."
     Invoke-WebRequest -Verbose -UseBasicParsing https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies/$($json.id) -Headers @{Authorization = "Bearer $($token)"} -ContentType "application/json" -Method PATCH -Body $($json | ConvertTo-Json -Depth 10)
-}
-
-Write-Output "Adding new policies from the /new_jsons/ folder"
-$new_jsons = Get-ChildItem "new_jsons/" -Recurse -Include *.json
-foreach ($new_json in $new_jsons) {
-    
-    $json = Get-Content $new_json.FullName | Out-String | ConvertFrom-Json
-    $json.PSObject.Properties.Remove('createdDateTime')
-    $json.PSObject.Properties.Remove('modifiedDateTime')
-    Write-Output "The json is $($json|ConvertTo-Json) ."
-    Invoke-WebRequest -Verbose -UseBasicParsing https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies -Headers @{Authorization = "Bearer $($token)"} -ContentType "application/json" -Method POST -Body $($json | ConvertTo-Json -Depth 10)
 }
 
